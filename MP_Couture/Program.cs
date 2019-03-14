b0f5a3aff19ba4a37db241b98d2210f235155989
mugdha pati <mugdhapati@gmail.com>
2019-03-13 20:30:19 -0700

    Initial Commit for MP_Couture

---
+++ MP_Couture/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MP_Couture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ MP_Couture/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+/// <summary>
+/// name of my ecommerce website for womens apparel is Pati_couture
+/// </summary>
+namespace MP_Couture
+{
+    /// <summary>
+    /// defining a class for a user to login to the ecommerce website
+    /// </summary>
+    class User
+    {
+        /// <summary>
+        /// general information of a user like userID, firstname, lastname, email, phonenumber
+        /// </summary>
+        public int UserID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public int PhoneNumber { get; set; }
+
+    }
+}
+
